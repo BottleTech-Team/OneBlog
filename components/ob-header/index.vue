eee74f85dfe72e69b3c09a01777c2594375edb69
zayfen <zhangyunfeng0101@gmail.com>
2019-07-24 21:07:37 +0800

    chore: commit code

--- components/ob-header/index.vue
+++ components/ob-header/index.vue
@@ -1,10 +1,27 @@
 <template>
-  
+  <div class="page-header">
+    <div class="container">
+      <img :src="header.icon">
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
-import Header from '@/models/page-header'
+
+export type MenuItem = {
+  name: String,
+  link: String
+}
+export type Menu = {
+  name: String,
+  items: Array<MenuItem>
+}
+
+export type Header = {
+  icon: String,
+  menus: Array<Menu>
+}
 
 @Component
 export default class PageHeader extends Vue {
